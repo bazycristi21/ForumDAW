939018ec23faa3af31d3e0f066cc51d22ff894fd
bazycristi21 <bazy_cristi_21@yahoo.com>
2021-01-10 10:12:51 +0200

    Full CRUD for Questions and Answers

--- ForumDAW/ForumDAW/Controllers/AnswersController.cs
+++ ForumDAW/ForumDAW/Controllers/AnswersController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using ForumDAW.Models;
 using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using System.Web.UI;
+
 namespace ForumDAW.Controllers
 {
     public class AnswersController : Controller
@@ -30,6 +33,7 @@ namespace ForumDAW.Controllers
                 {
                     answerRequest.PublishTime = DateTime.Now;
                     answerRequest.QuestionId = QuestionId;
+                    answerRequest.UserId = User.Identity.GetUserId();
                     db.Answers.Add(answerRequest);
                     db.SaveChanges();
                     return RedirectToAction("AllQuestions", "Questions");
@@ -42,5 +46,57 @@ namespace ForumDAW.Controllers
             }
             return View();
         }
+        [HttpGet]
+        public ActionResult Edit(int ? id)
+        {
+            if(id.HasValue)
+            {
+                Answer ans = db.Answers.Find(id);
+                if(ans == null)
+                {
+                    return HttpNotFound("Could't find it, id " + id.ToString());
+                }
+                return View(ans);
+            }
+            return HttpNotFound("Id is null");
+        }
+        [HttpPut]
+        public ActionResult ChangeContent(Answer answerRequest)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Answer ans = db.Answers.FirstOrDefault(b => b.Id.Equals(answerRequest.Id));
+                    if (TryUpdateModel(ans))
+                    {
+                        ans.Description = answerRequest.Description;
+
+                        db.SaveChanges(); 
+                    }
+                    return RedirectToAction("AllQuestions", "Questions");
+
+                }
+                return View(answerRequest);
+            }
+            catch(Exception e)
+            {
+                return View(answerRequest);
+            }
+
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Answer ans = db.Answers.Find(id);
+            if(ans != null)
+            {
+                db.Answers.Remove(ans);
+                db.SaveChanges();
+                return RedirectToAction("AllQuestions", "Questions");
+            }
+            return HttpNotFound("Couldn't find it, id: " + id.ToString());
+        }
+       
     }
 }
--- ForumDAW/ForumDAW/Controllers/QuestionsController.cs
+++ ForumDAW/ForumDAW/Controllers/QuestionsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ForumDAW.Models;
+using Microsoft.AspNet.Identity;
+
 namespace ForumDAW.Controllers
 {
     public class QuestionsController : Controller
@@ -34,6 +36,7 @@ namespace ForumDAW.Controllers
                 if (ModelState.IsValid)
                 {
                     questionRequest.PublishTime = DateTime.Now;
+                    questionRequest.UserId = User.Identity.GetUserId();
                     db.Questions.Add(questionRequest);
                     db.SaveChanges();
                     return RedirectToAction("AllQuestions");
@@ -47,12 +50,81 @@ namespace ForumDAW.Controllers
         }
         public ActionResult AllQuestions()
         {
+            List<ApplicationUser> users = db.Users.ToList();
+
             List<Question> questions = db.Questions.ToList();
+            foreach(var q in questions)
+            {
+                q.User = users.FirstOrDefault(u => u.Id == q.UserId);
+            }
             List<Answer> answers = db.Answers.ToList();
+            foreach (var a in answers)
+            {
+                a.User = users.FirstOrDefault(u => u.Id == a.UserId);
+            }
             //AllQuestionsAnswers A = new AllQuestionsAnswers { Questions = questions, Answers = answers };
             ViewBag.questions = questions;
             ViewBag.answers = answers;
             return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                Question ans = db.Questions.Find(id);
+                if (ans == null)
+                {
+                    return HttpNotFound("Could't find it, id " + id.ToString());
+                }
+                return View(ans);
+            }
+            return HttpNotFound("Id is null");
+        }
+        [HttpPut]
+        public ActionResult ChangeContent(Question questionRequest)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Question ans = db.Questions.FirstOrDefault(b => b.Id.Equals(questionRequest.Id));
+                    if (TryUpdateModel(ans))
+                    {
+                        ans.Description = questionRequest.Description;
+
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("AllQuestions", "Questions");
+
+                }
+                return View(questionRequest);
+            }
+            catch (Exception e)
+            {
+                return View(questionRequest);
+            }
+
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Question q = db.Questions.Find(id);
+            if (q != null)
+            {
+                List<Answer> answers = db.Answers.ToList();
+                foreach(var ans in answers)
+                {
+                    if(ans.QuestionId == q.Id)
+                    {
+                        db.Answers.Remove(ans);
+                    }
+                }
+                db.Questions.Remove(q);
+                db.SaveChanges();
+                return RedirectToAction("AllQuestions", "Questions");
+            }
+            return HttpNotFound("Couldn't find it, id: " + id.ToString());
         }
     }
 }
